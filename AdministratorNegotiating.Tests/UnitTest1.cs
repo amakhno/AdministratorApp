1909c12b417e5830cc3ed548cc67d4860b6a0f1b
Artem Makhno <artem_mahno@mail.ru>
2017-11-09 15:37:08 +0300

    UnitTests don't work

---
+++ AdministratorNegotiating.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdministratorNegotiating.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
